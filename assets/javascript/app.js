f2d83d9b5b878f881a286f5f86ca47afbeab060c
Carrington Rain Allison <Carrington_Allison@yahoo.com>
2018-10-18 00:36:33 -0700

    still not working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -11,13 +11,14 @@
 
   var database = firebase.database();
 
+  //fires when the submit button is clicked to add train
 $("#add-train-button").on("click", function(event) {
     event.preventDefault();
   
     // Grabs user input
     var trainName = $("#train-name-input").val().trim();
     var destination = $("#destination-input").val().trim();
-    var trainStart = moment($("#start-input").val().trim(), "hh:mm").format("X");
+    var trainStart = moment($("#start-input").val().trim(), "HH:mm").format("X");
     var trainRate = $("#rate-input").val().trim();
   
     // Creates local "temporary" object for holding employee data
@@ -40,38 +41,83 @@ $("#add-train-button").on("click", function(event) {
     $("#rate-input").val("");
   });
   
+  
+
+
+
+function GetClock(){
+var d=new Date();
+var nhour=d.getHours(),nmin=d.getMinutes(),nsec=d.getSeconds(),ap;
+
+if(nhour==0){ap=" AM";nhour=12;}
+else if(nhour<12){ap=" AM";}
+else if(nhour==12){ap=" PM";}
+else if(nhour>12){ap=" PM";nhour-=12;}
+
+if(nmin<=9) nmin="0"+nmin;
+if(nsec<=9) nsec="0"+nsec;
+
+var clocktext=""+nhour+":"+nmin+":"+nsec+ap+"";
+document.getElementById('clockbox').innerHTML=clocktext;
+}
+
+GetClock();
+setInterval(GetClock,1000);
+  
+  
+  
   // 3. Create Firebase event for adding employee to the database and a row in the html when a user adds an entry
   database.ref().on("child_added", function(childSnapshot) {
-    console.log(childSnapshot.val());
+      console.log(childSnapshot.val());
+      
+      // Store everything into a variable.
+      var trainName = childSnapshot.val().name;
+      var destination = childSnapshot.val().role;
+      var trainStart = childSnapshot.val().start;
+      var trainRate = childSnapshot.val().rate;
+
+    console.log(trainStart)
+      
+      var now = moment()
+      console.log(now)
+
+      var trainTimeConverted = moment(trainStart, "HH:mm").subtract(1, "years");
+      console.log(trainTimeConverted);
   
-    // Store everything into a variable.
-    var trainName = childSnapshot.val().name;
-    var destination = childSnapshot.val().role;
-    var trainStart = childSnapshot.val().start;
-    var trainRate = childSnapshot.val().rate;
+      // Current Time
+      var currentTime = moment();
+      console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
   
-    // Prettify the employee start
-    var trainStartPretty = moment.unix(trainStart).format("MM/DD/YYYY");
+      // Difference between the times
+      var diffTime = moment().diff(moment(trainTimeConverted), "minutes");
+      console.log("DIFFERENCE IN TIME: " + diffTime);
   
-    // Calculate the months worked using hardcore math
-    // To calculate the months worked
-    var empMonths = moment().diff(moment(trainStart, "X"), "months");
-    // console.log(empMonths);
+      // Time apart (remainder)
+      var tRemainder = diffTime % trainRate;
+      console.log(tRemainder);
   
-    // Calculate the total billed rate
-    var empBilled = empMonths * trainRate;
-    // console.log(empBilled);
+      // Minute Until Train
+      var minAway = trainRate - tRemainder;
+      console.log("MINUTES TILL TRAIN: " + minAway);
   
-    // Create the new row
-    var newRow = $("<tr>").append(
-      $("<td>").text(trainName),
-      $("<td>").text(destination),
-      $("<td>").text(trainStartPretty),
-      $("<td>").text(empMonths),
-      $("<td>").text(trainRate),
-      $("<td>").text(empBilled)
-    );
-  
-    // Append the new row to the table
-    $("#employee-table > tbody").append(newRow);
+      // Next Train
+      var nextTrain = moment().add(minAway, "minutes");
+      console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
+
+
+      // Prettify the employee start
+      var displayTrainStart = moment.unix(trainStart).format("hh:mm a");
+      
+      
+      // Create the new row
+      var newRow = $("<tr>").append(
+          $("<td>").text(trainName),
+          $("<td>").text(destination),
+          $("<td>").text(displayTrainStart),
+          $("<td>").text(trainRate + " mins"),
+          $("<td>").text(minAway + " mins"),
+          );
+          
+          // Append the new row to the table
+          $("#employee-table > tbody").append(newRow);
   });
